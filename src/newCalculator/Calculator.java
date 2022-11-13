1daeba77b9cd90e5eb1a19afff8f11fbf3a3a2e5
Abdulla <abdullacforu@gmail.com>
2022-11-14 03:06:40 +0530

    add percentage function and add invalidOperator#

--- src/newCalculator/Calculator.java
+++ src/newCalculator/Calculator.java
@@ -16,36 +16,16 @@ public class Calculator {
 	
 	
 	public static void main(String arg[]) {
-		
-//	
-//		////num1
-//		fNum1();
-//		
-//		////operator
-//		fOperator();
-//		
-//		////num2
-//		fNum2();
-//		
-//		////result
-//			int i=0,j=1;
-//			for(i=0;i<j;i++) {
-//		fResult(num1,num2,operatorS);
-//			j++;
-//			num1=result;
-//			fOperator();
-//			fNum2();
-//		}
-//			
+
 			
 			for(int i=1;i<2;i++) {
 				if(numb1==0) {
 					numb1=fNum1();
-				}else {
+				}else {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK10");
 					//somthing
 				}
 				String oprtS=fOperator();
-				if(oprtS.equals("=")) {
+				if(oprtS.equals("=")) {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK11");
 					//somthimg
 				}else {
 					numb2=fNum2();
@@ -55,21 +35,11 @@ public class Calculator {
 				numb1=rslt;
 				i=i-1;
 			}
-			
-			
-//			fResult(num1,num2,operatorS);
-//			fNum1();
-//			fOperator();
-//			fNum2();
-//			
-			
+		
 		
 	}
 	
 	
-
-
-
 
 
 
@@ -79,7 +49,7 @@ public class Calculator {
 		String num1S=sc.next().toString();
 		num1=Double.parseDouble(num1S);
 		}else {
-			num1=result;
+			num1=result;System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK9");
 		}
 		return num1;
 	}
@@ -88,15 +58,18 @@ public class Calculator {
 		for(int i=1;i<2;i++) {
 		System.out.println("Enter a Operator@");
 		operatorS=sc.next().toString();
-		if(operatorS.equals("=")||operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")) {
-				if(operatorS.equals("=")) {
+		if(operatorS.equals("=")||operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")||operatorS.equals("%")) {
+				if(operatorS.equals("=")) {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK8");
 					rslt=numb1;  //  (numb1 value+numb2 0)
-					System.out.println("Result is "+rslt);
-				}else{
+					fDesplay(rslt);
+				}else if(operatorS.equals("%")){
+					rslt=numb1/100;
+					fDesplay(rslt);
+				}else {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK7");
 					
 				}
 		}else {
-			System.out.println("invalid operator#");
+			System.out.println("invalid operator@");
 			i=i-1;
 		}
 		}
@@ -106,25 +79,28 @@ public class Calculator {
 	static double fNum2() {
 		System.out.println("Enter a second number");
 		String num2S=sc.next().toString();
-		num2=Integer.parseInt(num2S);
+		num2=Double.parseDouble(num2S);
 		return num2;
 	}
 	
 	static double fResult(double numbr1, String oprtrS, double numbr2) {
 		double rsltOprton=0;
-		System.out.println("Enter a Operator#");
+		System.out.println("Enter a Operator#");System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK5");
 		operatorS=sc.next().toString();
 		if(operatorS.equals("=")) {
-			rsltOprton=fOperation(numbr1,oprtrS,numbr2);
+			rsltOprton=fOperation(numbr1,oprtrS,numbr2);System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK4");
 		}else if(operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")){
-			rsltOprton=fOperation(numbr1,oprtrS,numbr2);
-			
+			rsltOprton=fOperation(numbr1,oprtrS,numbr2);System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK3");
+		}else if(operatorS.equals("%")){
+			rsltOprton=fPercentage(numbr1,oprtrS,numbr2);//rsltOprton*100;
+		}else {
+			System.out.println("invalid operator#");
 		}
-	return rsltOprton;
+	return rsltOprton; 
 }
 
-	static double fOperation(double numbr1, String oprtrS, double numbr2) {
-		if(oprtrS.equals("+")) {
+	static double fOperation(double numbr1, String oprtrS, double numbr2) {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK2");
+			if(oprtrS.equals("+")) {
 			 result=numbr1+numbr2;
 			}else if(oprtrS.equals("-")) {
 				result=numbr1-numbr2;
@@ -132,85 +108,31 @@ public class Calculator {
 				result=numbr1*numbr2;
 			}else if(oprtrS.equals("/")) {
 				result=numbr1/numbr2;
+			}else {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK6");
+				//System.out.println("invalid operator");
+			}
+		return result;  
+	}
+	
+	static double fPercentage(double numbr1, String oprtrS, double numbr2){double preResult=0;
+		if(oprtrS.equals("+")) {
+			 	result=numbr1+(numbr2/100);
+			}else if(oprtrS.equals("-")) {
+				result=numbr1-(numbr2/100);
+			}else if(oprtrS.equals("*")) {
+				result=numbr1*(numbr2/100);
+			}else if(oprtrS.equals("/")) {
+				result=numbr1/(numbr2/100);
 			}else {
 				//System.out.println("invalid operator");
 			}
-		return result;
+		return result;  
 	}
 
 	static void fDesplay(double rsult) {
-		System.out.println(rsult);
+		System.out.println(rsult);System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK1");
 	}
 
-	
-	
-	
-	
-//	
-//	
-//	static void fNum1() {
-//		System.out.println("Enter a number");
-//		num1S=sc.next().toString();
-//		num1=Integer.parseInt(num1S);
-//	}
-//	
-//	static void fOperator() {
-//		if(oprtr==false) {
-//			System.out.println("Enter a Operator");
-//			operatorS=sc.next().toString();
-//			oprtr=true;
-//		}else {
-////			if(operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")) {
-////			int i=0,j=1;
-////			for(i=0;i<j;i++) {
-//		fResult(num1,num2,operatorS);
-////			j++;
-//			num1=result;
-//			System.out.println("Entered operator "+operatorS);
-//			fNum2();
-////			}
-////		  }
-//		}
-//	}
-//	
-//	static void fNum2() {
-//		System.out.println("Enter a number");
-//		num2S=sc.next().toString();
-//		num2=Integer.parseInt(num2S);
-//	}
-//	
-//	static void fResult(int numb1, int numb2, String operatorb) {
-//		System.out.println(numb1+"  "+operatorb+"  "+numb2);
-//		if(oprtr=false){ 
-//	  System.out.println("enter a operator or =");
-//	  resultS=sc.next().toString();
-//		}else {
-//			
-//		}
-//	  if(resultS.equals("=")) {
-//		
-//		if(operatorb.equals("+")) {
-//		 result=numb1+numb2;
-//			System.out.println(result);
-//		}else if(operatorb.equals("-")) {
-//			result=numb1-numb2;
-//			System.out.println(result);
-//		}else if(operatorb.equals("*")) {
-//			result=numb1*numb2;
-//			System.out.println(result);
-//		}else if(operatorb.equals("/")) {
-//			result=numb1/numb2;
-//			System.out.println(result);
-//		}else {
-//			System.out.println("invalid operator");
-//		}
-//		resultS="x=x";
-//	  }else if(operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")){
-//		//  fOperator();
-//	  }else {
-//		  System.out.println("invalid operator");
-//	  }
-//	}
 	
 	
 }
