cc773fc010ec667fde25eac2b466e2e06c8b21b5
Abdulla <abdullacforu@gmail.com>
2022-11-14 14:10:42 +0530

    add array and some

--- src/newCalculator/Calculator.java
+++ src/newCalculator/Calculator.java
@@ -12,36 +12,55 @@ public class Calculator {
 	static double num1=0,num2=0,result=0;
 	static String oprtS="";
 	static double numb1=0,numb2=0,rslt=0;
-	
+	static String[] arrMemory=new String[100];
+	static int arrLastPos=0;
+
 	
 	
 	public static void main(String arg[]) {
+		
+			preSetArrMemory();
 
 			
 			for(int i=1;i<2;i++) {
 				if(numb1==0) {
-					numb1=fNum1();
-				}else {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK10");
+					numb1=fNum1();  
+					String sNumb1=String.valueOf(numb1);
+					addArrMemory(sNumb1);
+				}else {
 					//somthing
 				}
-				String oprtS=fOperator();
-				if(oprtS.equals("=")) {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK11");
+				String oprtS=fOperator();     
+					String sOprt=String.valueOf(oprtS);  
+					addArrMemory(sOprt);
+				if(oprtS.equals("=")) {
 					//somthimg
 				}else {
-					numb2=fNum2();
+					numb2=fNum2();      
+						String sNumb2=String.valueOf(numb2);
+						addArrMemory(sNumb2);
 					rslt=fResult(numb1, oprtS, numb2);
-					fDesplay(rslt);
+					fDesplay(rslt);      
+						String sRslt=String.valueOf(rslt);  
+						addArrMemory(sRslt);
 				}
 				numb1=rslt;
 				i=i-1;
+				readArrMemory();
 			}
 		
-		
+	
 	}
 	
 	
 
 
+
+	private static void preSetArrMemory() {
+		for(int i=0;i<20;i++) {
+			 arrMemory[i]="dummy";
+		}
+	}
 
 	static double fNum1() {
 		if(num1==0) {
@@ -49,7 +68,7 @@ public class Calculator {
 		String num1S=sc.next().toString();
 		num1=Double.parseDouble(num1S);
 		}else {
-			num1=result;System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK9");
+			num1=result;
 		}
 		return num1;
 	}
@@ -59,13 +78,22 @@ public class Calculator {
 		System.out.println("Enter a Operator@");
 		operatorS=sc.next().toString();
 		if(operatorS.equals("=")||operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")||operatorS.equals("%")) {
-				if(operatorS.equals("=")) {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK8");
-					rslt=numb1;  //  (numb1 value+numb2 0)
-					fDesplay(rslt);
+				if(operatorS.equals("=")) {
+						String sOperator=String.valueOf(operatorS);  
+						addArrMemory(sOperator);
+					numb2=0;          
+						String sNumb2=String.valueOf(numb2);
+						addArrMemory(sNumb2);
+					rslt=numb1+numb2;  //  (numb1 value+numb2 0)
+					fDesplay(rslt);   
+						String sRslt=String.valueOf(rslt);
+						addArrMemory(sRslt);
 				}else if(operatorS.equals("%")){
 					rslt=numb1/100;
 					fDesplay(rslt);
-				}else {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK7");
+						String sRslt=String.valueOf(rslt);
+						addArrMemory(sRslt);
+				}else {
 					
 				}
 		}else {
@@ -85,13 +113,17 @@ public class Calculator {
 	
 	static double fResult(double numbr1, String oprtrS, double numbr2) {
 		double rsltOprton=0;
-		System.out.println("Enter a Operator#");System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK5");
+		System.out.println("Enter a Operator#");
 		operatorS=sc.next().toString();
 		if(operatorS.equals("=")) {
-			rsltOprton=fOperation(numbr1,oprtrS,numbr2);System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK4");
+				String sOperator=String.valueOf(operatorS);  
+				addArrMemory(sOperator);
+			rsltOprton=fOperation(numbr1,oprtrS,numbr2);
 		}else if(operatorS.equals("+")||operatorS.equals("-")||operatorS.equals("*")||operatorS.equals("/")){
-			rsltOprton=fOperation(numbr1,oprtrS,numbr2);System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK3");
+			rsltOprton=fOperation(numbr1,oprtrS,numbr2);
 		}else if(operatorS.equals("%")){
+				String sOperator=String.valueOf(operatorS);  
+				addArrMemory(sOperator);
 			rsltOprton=fPercentage(numbr1,oprtrS,numbr2);//rsltOprton*100;
 		}else {
 			System.out.println("invalid operator#");
@@ -99,7 +131,7 @@ public class Calculator {
 	return rsltOprton; 
 }
 
-	static double fOperation(double numbr1, String oprtrS, double numbr2) {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK2");
+	static double fOperation(double numbr1, String oprtrS, double numbr2) {
 			if(oprtrS.equals("+")) {
 			 result=numbr1+numbr2;
 			}else if(oprtrS.equals("-")) {
@@ -108,7 +140,7 @@ public class Calculator {
 				result=numbr1*numbr2;
 			}else if(oprtrS.equals("/")) {
 				result=numbr1/numbr2;
-			}else {System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK6");
+			}else {
 				//System.out.println("invalid operator");
 			}
 		return result;  
@@ -130,9 +162,26 @@ public class Calculator {
 	}
 
 	static void fDesplay(double rsult) {
-		System.out.println(rsult);System.out.println("KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK1");
+		System.out.println(rsult);
 	}
 
+	static void addArrMemory(String addValeuS){
+		  arrMemory[arrLastPos]=addValeuS; arrLastPos=arrLastPos+1;     /////////////////
+	}
+	
+	static void readArrMemory() {
+		String mergeValueS="";
+		for(int i=0;i<20;i++) {
+		String readValueS=arrMemory[i];
+		if(readValueS.equals("dummy")) {
+			//somthing
+		}else {
+			
+			mergeValueS=mergeValueS+readValueS;
+		}
+		}
+		System.out.println(mergeValueS);
+	}
 	
 	
 }
